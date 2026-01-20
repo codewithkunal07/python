2ae954dd8c30f7c2d06c0d7b27c61ea70550e5b9
kunal <ds.kunal.rajput@gmail.com>
2026-01-20 17:21:11 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,43 @@
+name = "kunal"
+age = 19
+marks = 95.5
+#print function 
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)  
+
+#data types
+print (type("name"))
+print(type("age"))
+print(type("marks"))
+
+
+#data types
+a = ("kunal")
+b = 19
+c = None
+d=False
+print(type(a))
+print(type(b))          
+print(type(c))
+print(type(d))
+
+#opreatars
+a = 10
+b = 20
+print ((a<b) and (a==b ))
+
+#type casting
+a= "2"
+b=2.5
+print (int(a)+float         (b))
+
+
+#input function
+NAME = int(input("ENTER YOUR NAME :"))
+print(type(NAME),NAME)
+
+
+
+name=int(input("ENTER YOUR age :"))
+print(type(name),name)
